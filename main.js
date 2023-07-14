1a85ef3fb41c5d8e8b0ac9af969231c11222c355
saanvi <95546881+1789038@users.noreply.github.com>
2023-07-13 21:32:58 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function cg(){
+    window.location="chewinggum.html";
+}
+
+function wb(){
+    window.location="waterbottle.html";
+}
+
+function p(){
+    window.location="perfume.html";
+}
+
+function hp(){
+    window.location="headphones.html";
+}
+
+function hc(){
+    window.location="handcream.html";
+}
+
+function pc(){
+    window.location="pencil.html";
+}
